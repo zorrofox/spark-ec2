d3d67fcf2c9b00a6c932dbea786f6f51e9edb13b
zorrofoxH <debin.huang@gmail.com>
2015-12-31 11:31:34 +0800

    Commit for tachyon download url

--- tachyon/init.sh
+++ tachyon/init.sh
@@ -16,44 +16,44 @@ then
 else
   case "$TACHYON_VERSION" in
     0.3.0)
-      wget https://s3.amazonaws.com/Tachyon/tachyon-0.3.0-bin.tar.gz
+      wget ${DOWNLOAD_URL}Tachyon/tachyon-0.3.0-bin.tar.gz
       ;;
     0.4.0)
-      wget https://s3.amazonaws.com/Tachyon/tachyon-0.4.0-bin.tar.gz
+      wget ${DOWNLOAD_URL}Tachyon/tachyon-0.4.0-bin.tar.gz
       ;;
     0.4.1)
-      wget https://s3.amazonaws.com/Tachyon/tachyon-0.4.1-bin.tar.gz
+      wget ${DOWNLOAD_URL}Tachyon/tachyon-0.4.1-bin.tar.gz
       ;;
     0.5.0)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget https://s3.amazonaws.com/Tachyon/tachyon-0.5.0-bin.tar.gz
+        wget ${DOWNLOAD_URL}Tachyon/tachyon-0.5.0-bin.tar.gz
       else
-        wget https://s3.amazonaws.com/Tachyon/tachyon-0.5.0-cdh4-bin.tar.gz
+        wget ${DOWNLOAD_URL}Tachyon/tachyon-0.5.0-cdh4-bin.tar.gz
       fi
       ;;
     0.6.0)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.0-bin.tar.gz
+        wget ${DOWNLOAD_URL}Tachyon/tachyon-0.6.0-bin.tar.gz
       else
-        wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.0-cdh4-bin.tar.gz
+        wget ${DOWNLOAD_URL}Tachyon/tachyon-0.6.0-cdh4-bin.tar.gz
       fi
       ;;
     0.6.4)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.4-bin.tar.gz
+        wget ${DOWNLOAD_URL}Tachyon/tachyon-0.6.4-bin.tar.gz
       elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
-        wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.4-cdh4-bin.tar.gz
+        wget ${DOWNLOAD_URL}Tachyon/tachyon-0.6.4-cdh4-bin.tar.gz
       else
-        wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.4-hadoop2.4-bin.tar.gz
+        wget ${DOWNLOAD_URL}Tachyon/tachyon-0.6.4-hadoop2.4-bin.tar.gz
       fi
       ;;
     *)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget https://s3.amazonaws.com/Tachyon/tachyon-$TACHYON_VERSION-bin.tar.gz
+        wget ${DOWNLOAD_URL}Tachyon/tachyon-$TACHYON_VERSION-bin.tar.gz
       elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
-        wget https://s3.amazonaws.com/Tachyon/tachyon-$TACHYON_VERSION-cdh4-bin.tar.gz
+        wget ${DOWNLOAD_URL}Tachyon/tachyon-$TACHYON_VERSION-cdh4-bin.tar.gz
       else
-        wget https://s3.amazonaws.com/Tachyon/tachyon-$TACHYON_VERSION-hadoop2.4-bin.tar.gz
+        wget ${DOWNLOAD_URL}Tachyon/tachyon-$TACHYON_VERSION-hadoop2.4-bin.tar.gz
       fi
       if [ $? != 0 ]; then
         echo "ERROR: Unknown Tachyon version"
